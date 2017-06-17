ba2ee43c9ac535ee585a6f6afd497a50fa98c0c0
Diego Rueda <sandiego206@gmail.com>
2017-06-17 13:06:07 -0500

    move TodoDataService to module, instead of component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  providers: []
 })
 
 export class AppComponent {
