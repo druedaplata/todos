6ce69d7e278eda5f050bd7cbe067b1dff430630d
Diego Rueda <sandiego206@gmail.com>
2017-06-17 12:21:54 -0500

    create todo input front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+import { Todo } from './todo';
+import { TodoDataService } from './todo-data.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+  newTodo: Todo = new Todo();
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token 'TodoDataService'
+  // and assign it to a property called 'todoDataService'
+  constructor(private todoDataService: TodoDataService) {
+
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  // Service is now available as this.todoDataService
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  getTodos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
